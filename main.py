f4762d55508321b2c30431b47852c33021ad7c4a
gauyex-bzz <gauyex@bzz.ch>
2024-05-14 08:53:50 +0200

    UPDATED: main.py animal can be interacted with

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 animal = input('Please choose one of the following pets: cat, dog, rabbit or hamster.\n')
 
 while animal not in ['cat', 'dog', 'rabbit', 'hamster']:
@@ -8,7 +10,40 @@ while animal not in ['cat', 'dog', 'rabbit', 'hamster']:
 name = input(f'Please enter a name for your {animal}\n')
 keep = input(f'Congrats! You now own a {animal} named {name}! Would you like to keep it? (yes/no)\n')
 
+friendshipLevel = 0
+
 if keep == 'yes':
-    print(f'Great! You can now interact with {name}!')
+    print(f'Great! You can now interact with {name}! Your friendship level is {friendshipLevel}.')
 else:
     print(f'{name} was released.')
+    exit()
+
+while keep:
+    interact = input(f'Would you like to interact with {name}? (yes/no)\n')
+    while interact == 'no':
+        time.sleep(5)
+        interact = input(f'Would you like to interact with {name} now? (yes/no)\n')
+        if interact == 'yes':
+            break
+    interactionType = input(f'What would you like to do with {name}? (feed, play, swim, sleep, release, sell, kill)\n')
+    if interactionType == 'feed':
+        print(f'{name} was fed.')
+        friendshipLevel += 0.2
+    elif interactionType == 'play':
+        print(f'{name} played with you.')
+        friendshipLevel += 0.1
+    elif interactionType == 'swim':
+        print(f'{name} swam.')
+        friendshipLevel += 0.1
+    elif interactionType == 'sleep':
+        print(f'{name} slept.')
+        friendshipLevel += 0.1
+    elif interactionType == 'release':
+        print(f'{name} was released. You can choose another pet by restarting the game.')
+        exit()
+    elif interactionType == 'sell':
+        print(f'{name} was sold for $3. Feel happy now?')
+        exit()
+    elif interactionType == 'kill':
+        print(f'{name} was killed. I hope you feel bad.')
+        exit()
