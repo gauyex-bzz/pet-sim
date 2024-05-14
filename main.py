427b1e15cbc05751f83f746917ecf4a6f2046826
gauyex-bzz <gauyex@bzz.ch>
2024-05-14 09:39:15 +0200

    UPDATED: main.py animal can be hungry

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ name = input(f'Please enter a name for your {animal}\n')
 keep = input(f'Congrats! You now own a {animal} named {name}! Would you like to keep it? (yes/no)\n')
 
 friendshipLevel = 9.9
+feed = 0
 
 if keep == 'yes':
     print(f'Great! You can now interact with {name}! Your friendship level is {friendshipLevel}.')
@@ -18,34 +19,56 @@ else:
     print(f'{name} was released.')
     exit()
 
+
+def hungry():
+    print(f'{name} is hungry and does not want to {interactionType}. You should feed it.')
+
+
+def friendship_up():
+    global friendshipLevel
+    if friendshipLevel < 10:
+        friendshipLevel += 0.1
+
+
 while keep:
     if friendshipLevel in [2, 3, 4, 5, 6, 7, 8, 9, 10]:
         print(f'Congrats! Your friendship level is now {friendshipLevel}.')
     if friendshipLevel == 10:
         print(f'You have reached the maximum friendship level with {name}!')
-    interact = input(f'Would you like to interact with {name}? (yes/no)\n')
+    interact = input(f'\nWould you like to interact with {name}? (yes/no)\n')
     while interact == 'no':
         time.sleep(5)
-        interact = input(f'Would you like to interact with {name} now? (yes/no)\n')
+        interact = input(f'\nWould you like to interact with {name} now? (yes/no)\n')
         if interact == 'yes':
             break
     interactionType = input(f'What would you like to do with {name}? (feed, play, swim, sleep, release, sell, kill)\n')
     if interactionType == 'feed':
         print(f'{name} was fed.')
-        if friendshipLevel < 10:
-            friendshipLevel += 0.2
+        friendship_up()
+        feed += 1
+        if feed == 5:
+            print(f'{name} is now fat. You should stop feeding it.')
+        if feed == 10:
+            print(f'{name} is now obese. Please stop feeding that poor {animal}.')
+        if feed == 15:
+            print(f'{name} is now dead. You killed it by overfeeding it. What a horrible death.')
+            exit()
     elif interactionType == 'play':
-        print(f'{name} played with you.')
-        if friendshipLevel < 10:
-            friendshipLevel += 0.1
+        if feed < 1:
+            hungry()
+        else:
+            print(f'{name} played with you.')
+            friendship_up()
     elif interactionType == 'swim':
-        print(f'{name} swam.')
-        if friendshipLevel < 10:
-            friendshipLevel += 0.1
+        if feed < 1:
+            hungry()
+        else:
+            print(f'{name} swam.')
+            friendship_up()
     elif interactionType == 'sleep':
         print(f'{name} slept.')
-        if friendshipLevel < 10:
-            friendshipLevel += 0.1
+        friendship_up()
+        feed = 2
     elif interactionType == 'release':
         print(f'{name} was released. You can choose another pet by restarting the game.')
         exit()
