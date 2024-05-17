5135a7653c6a328208a3135e4b7b41b6c456eb83
gauyex-bzz <gauyex@bzz.ch>
2024-05-17 13:29:36 +0200

    UPDATED: version2.py created select function

--- version2.py
+++ version2.py
@@ -3,79 +3,122 @@ import random
 
 class Pet:
     def __init__(self, name, animal):
-        self.name = name
-        self.animal = animal
-        self.feed = 3
-        self.friendship = 0
+        self._name = name
+        self._animal = animal
+        self._feed = 3
+        self._friendship = 0
 
     def friendship_up(self):
         self.friendship += 0.1
 
-    def hungry(self, interaction):
-        print(f'{self.name} is too hungry to {interaction}. You should feed it.')
+    def friendship_down(self):
+        self.friendship -= 1
 
-    def interaction(self, interaction_type):
-        if interaction_type == 'feed':
-            amount = int(input(f'How much would you like to feed {self.name}? (1-3)\n'))
-            print(f'{self.name} was fed.')
-            self.friendship_up()
-            self.feed += amount
-            if self.feed == 8:
-                print(f'{self.name} is now fat. You should stop feeding it.')
-            if self.feed == 13:
-                print(f'{self.name} is now obese. Please stop feeding that poor {self.animal}.')
-            if self.feed == 18:
-                print(f'{self.name} is now dead. You killed it by overfeeding it. What a horrible death.')
+    def hungry(self):
+        print(f'{self.name} is too hungry to do that. You should feed it.')
+
+    def hunger(self):
+        self.feed -= 1
+
+    def feed(self):
+        print(f'{self.name} was fed.')
+        amount = int(input('How much food would you like to give it? (1-3)'))
+        self.friendship_up()
+        self.feed += amount
+        if self.feed >= 8:
+            print(f'{self.name} is now fat. You should stop feeding it.')
+        if self.feed >= 13:
+            print(f'{self.name} is now obese. Please stop feeding that poor {self.animal}.')
+        if self.feed >= 18:
+            print(f'{self.name} is now dead. You killed it by overfeeding it. What a horrible death.')
+            exit()
+
+    def play(self):
+        break_neck = random.randint(1, 1000)
+        hit_by_car = random.randint(1, 1000)
+        if self.feed < 1:
+            self.hungry()
+        else:
+            if break_neck == 1:
+                print(f'{self.name} broke its neck while playing. What the hell were you doing?.')
                 exit()
-
-        elif interaction_type == 'play':
-            breakNeck = random.randint(1, 1000)
-            if self.feed < 1:
-                self.hungry(interaction_type)
+            elif hit_by_car == 1:
+                print(f'{self.name} was hit by a car while playing. Better watch out next time.')
+                exit()
             else:
-                if breakNeck == 1:
-                    print(f'{self.name} broke its neck while playing. You are a terrible pet owner.')
-                    exit()
-                else:
-                    print(f'{self.name} played with you.')
-                    self.friendship_up()
-                    self.feed -= 0.5
+                print(f'{self.name} played with you.')
+                self.friendship_up()
+                self.feed -= 0.5
 
-        elif interaction_type == 'swim':
-            drown = random.randint(1, 1000)
-            if self.feed < 1:
-                self.hungry(interaction_type)
+    def swim(self):
+        drown = random.randint(1, 1000)
+        if self.feed < 1:
+            self.hungry()
+        else:
+            print(f'{self.name} swam.')
+            if drown == 1:
+                print(f'{self.name} drowned. You are a terrible pet owner.')
+                exit()
             else:
-                print(f'{self.name} swam.')
-                if drown == 1:
-                    print(f'{self.name} drowned. You are a terrible pet owner.')
-                    exit()
                 self.friendship_up()
                 self.feed = self.feed - 0.5
 
-        elif interaction_type == 'sleep':
-            if self.feed < 2:
-                print(f'{self.name} is too hungry to sleep. You should feed it.')
-            else:
-                print(f'{self.name} slept.')
-                self.friendship_up()
-                self.feed = self.feed - 2
+    def sleep(self):
+        if self.feed < 3:
+            print(f'{self.name} is too hungry to sleep. You should feed it.')
+        else:
+            print(f'{self.name} slept.')
+            self.friendship_up()
+            self.feed = self.feed - 2
 
-        elif interaction_type == 'release':
-            print(f'{self.name} was released. You can choose another pet by restarting the game.')
+    def release(self):
+        print(f'{self.name} was released. It is now free.')
+        exit()
+
+    def hit(self):
+        print(f'You hit {self.name}. This is animal abuse. Shame on you.')
+        self.friendship_down()
+        self.hunger()
+        if self.friendship < 0:
+            print(f'{self.name} ran away because it got scared of you.')
             exit()
+        elif self.feed < 1:
+            print(f'{self.name} died becaue it was too weak. You killed your pet. Shame on you.')
 
-        elif interaction_type == 'sell':
-            print(f'{self.name} was sold for $3. Feel happy now?')
-            exit()
+    @property
+    def name(self):
+        return self._name
 
-        elif interaction_type == 'hit':
-            print(f'{self.name} was hit.')
-            self.friendship -= 1
-            if self.friendship < 0:
-                print(f'{self.name} ran away because it got scared of you. You are a monster.')
-                exit()
+    @property
+    def animal(self):
+        return self._animal
 
-        if self.feed < 0:
-            print(f'{self.name} starved to death. You are a terrible pet owner.')
-            exit()
+    @property
+    def feed(self):
+        return self._feed
+
+    @feed.setter
+    def feed(self, value):
+        self._feed = value
+
+    @property
+    def friendship(self):
+        return self._friendship
+
+    @friendship.setter
+    def friendship(self, value):
+        self._friendship = value
+
+    def __str__(self):
+        return f'Name: {self.name}\n' \
+               f'Animal: {self.animal}\n' \
+               f'Feed: {self.feed}\n' \
+               f'Friendship: {self.friendship}\n'
+
+
+def select_pet(list):
+    if not list:
+        print('No pets available.')
+        return None
+
+
